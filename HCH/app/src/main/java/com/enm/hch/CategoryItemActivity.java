4c71a2ea038905f10e097c68eaf44db4b3cbc80e
enmiller94 <millerev17@hanover.edu>
2016-11-11 18:07:29 -0500

    Category Listing Working
    Category Listing is working. Interface isn't pretty, but functionality
    is intact.

--- HCH/app/src/main/java/com/enm/hch/CategoryItemActivity.java
+++ HCH/app/src/main/java/com/enm/hch/CategoryItemActivity.java
@@ -1,5 +1,90 @@
 package com.enm.hch;
 
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.view.View;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.CursorAdapter;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+public class CategoryItemActivity extends ListActivity {
+    private SQLiteDatabase db;
+    private Cursor cursor;
+
+    public static final String ITEM = "item";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ListView listSites = getListView();
+
+        int item = (Integer)getIntent().getExtras().get(ITEM);
+
+        try{
+            SQLiteOpenHelper HCHDatabaseHelper = new HCHDatabaseHelper(this);
+            db = HCHDatabaseHelper.getReadableDatabase();
+
+            cursor = db.query("SITE_TYPE",
+                    new String[]{"_id", "CATEGORY", "CATEGORY_PROPER"},
+                    "_id = ?",
+                    new String[] {Integer.toString(item)},
+                    null, null, null);
+
+            cursor.moveToFirst();
+            String category = cursor.getString(1);
+            String category_proper = cursor.getString(2);
+
+            String category_input = category + " = ?";
+
+            cursor = db.query("SITES_TO_SITE_TYPE",
+                    null,
+                    category_input,
+                    new String[]{"Y"},
+                    null,
+                    null,
+                    "SITE_NAME ASC");
+
+            CursorAdapter listAdapter = new SimpleCursorAdapter(this,
+                    android.R.layout.simple_list_item_1,
+                    cursor,
+                    new String[]{"SITE_NAME"},
+                    new int[]{android.R.id.text1},
+                    0);
+
+            listSites.setAdapter(listAdapter);
+
+        } catch(SQLiteException error) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        db.close();
+    }
+
+    @Override
+    public void onListItemClick(ListView listView,
+                                View itemView,
+                                int position,
+                                long id) {
+        Intent intent = new Intent(CategoryItemActivity.this, AlphabeticalItemActivity.class);
+        intent.putExtra(AlphabeticalItemActivity.ITEM, (int) id);
+        startActivity(intent);
+    }
+
+/*
+package com.enm.hch;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -50,4 +135,5 @@ public class CategoryItemActivity extends Activity {
             toast.show();
         }
     }
+*/
 }
