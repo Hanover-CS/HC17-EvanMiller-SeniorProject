6c480e27b972f54441639faf0df9f395b9d16171
enmiller94 <millerev17@hanover.edu>
2016-12-21 22:09:30 -0500

    Map and Database Integration
    Integration between the map and database is complete: click on site's
    icon -> open's small window with site name -> click on window -> opens
    activity with site's info. Created new activity
    RoamCampusSitesItemActivity.xml; practically identical to
    SitesItemActivity.xml except Item being passed thru is the SITE_NAME
    instead of an _id.

--- HCH/app/src/main/java/com/enm/hch/RoamCampusMapsActivity.java
+++ HCH/app/src/main/java/com/enm/hch/RoamCampusMapsActivity.java
@@ -1,12 +1,11 @@
 package com.enm.hch;
 
-import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
@@ -15,9 +14,7 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.location.LocationListener;
 import android.util.Log;
-import android.widget.TextView;
 import android.widget.Toast;
-
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -25,18 +22,20 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import static com.enm.hch.R.id.map;
 
 public class RoamCampusMapsActivity extends FragmentActivity implements
-        OnMapReadyCallback {
+        OnMapReadyCallback,
+        GoogleMap.OnMarkerClickListener,
+        GoogleMap.OnInfoWindowClickListener,
+        GoogleMap.OnInfoWindowLongClickListener,
+        GoogleMap.OnInfoWindowCloseListener {
 
     private GoogleMap mMap;
-    private Location locationLL;
-    private boolean bound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,81 +47,57 @@ public class RoamCampusMapsActivity extends FragmentActivity implements
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         //Create Map
         mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
         //Display Zoom Buttons
         UiSettings mapSettings;
         mapSettings = mMap.getUiSettings();
         mapSettings.setZoomControlsEnabled(true);
 
-        //Set Standing Marker
-        LatLng campus = new LatLng(38.71433, -85.48113);
-        mMap.addMarker(new MarkerOptions().position(campus).title("Campus Center").snippet("Campus Center"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(campus));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        //Set listeners for marker and info window events
+        mMap.setOnMarkerClickListener(this);
+        mMap.setOnInfoWindowClickListener(this);
+        mMap.setOnInfoWindowCloseListener(this);
+        mMap.setOnInfoWindowLongClickListener(this);
+
+        mMap.setMyLocationEnabled(true);
+
         addSiteMarkers();
 
-        /*
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(38.71433, -85.48113);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(14));
-        */
-
+        //Handler to continuously find device's current location and realign camera
         final Handler handler = new Handler();
         handler.post(new Runnable() {
             @Override
             public void run() {
-                /*
-                LatLng sydney = new LatLng(location.getLatitude(), location.getLongitude());
-                mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-                handler.postDelayed(this, 10000);
-                */
-
-                // Enable MyLocation Layer of Google Map
-                mMap.setMyLocationEnabled(true);
-                // Get LocationManager object from System Service LOCATION_SERVICE
+                //Code needed to pull device's current location
                 LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-                // Create a criteria object to retrieve provider
                 Criteria criteria = new Criteria();
-                // Get the name of the best provider
                 String provider = locationManager.getBestProvider(criteria, true);
-                // Get Current Location
                 Location myLocation = locationManager.getLastKnownLocation(provider);
-                // set map type
-                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                // Get latitude of the current location
+
+                //Find latitude & longitude
                 //double latitude = myLocation.getLatitude();
                 double latitude = 38.71433;
-                // Get longitude of the current location
                 //double longitude = myLocation.getLongitude();
                 double longitude = -85.48113;
-                // Create a LatLng object for the current location
+                //Create a LatLng object for the current location
                 LatLng latLng = new LatLng(latitude, longitude);
-                // Show the current location in Google Map
+
+                mMap.addMarker(new MarkerOptions().position(latLng));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-                // Zoom in the Google Map
                 mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
-                mMap.addMarker(new MarkerOptions().position(latLng).title("Hanover College").snippet("Panther Pride"));
+
+                //handler.postDelayed(this, 10000);
             }
         });
     }
 
     private void addSiteMarkers() {
+        //Pull LATITUDE and LONGITUDE from each SITE_NAME in SITES and populate markers for each
         try{
             SQLiteOpenHelper HCHDatabaseHelper = new HCHDatabaseHelper(this);
             SQLiteDatabase db = HCHDatabaseHelper.getReadableDatabase();
@@ -137,22 +112,28 @@ public class RoamCampusMapsActivity extends FragmentActivity implements
             double latitudeText = cursor.getDouble(2);
             double longitudeText = cursor.getDouble(3);
             LatLng latLng = new LatLng(latitudeText, longitudeText);
-            mMap.addMarker(new MarkerOptions().position(latLng).title(siteNameText).snippet(siteNameText));
+            Marker marker = mMap.addMarker(new MarkerOptions().position(latLng).title(siteNameText)
+                    .snippet("Hanover College")
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.hch_marker)));
+            //Sets Tag as SITE_NAME -> pass on to pull info from Database
+            marker.setTag(siteNameText);
 
             while (cursor.moveToNext()){
-                //cursor.moveToNext())
                 siteNameText = cursor.getString(1);
                 latitudeText = cursor.getDouble(2);
                 longitudeText = cursor.getDouble(3);
                 latLng = new LatLng(latitudeText, longitudeText);
-                //mMap.addCircle(new CircleOptions().center(latLng).radius(6).strokeColor(Color.BLUE).fillColor(Color.BLUE));
-                mMap.addMarker(new MarkerOptions().position(latLng).title(siteNameText).snippet(siteNameText)
+
+                //Creates Marker
+                marker = mMap.addMarker(new MarkerOptions().position(latLng).title(siteNameText)
+                        .snippet("Hanover College")
                         .icon(BitmapDescriptorFactory.fromResource(R.drawable.hch_marker)));
+                //Sets Tag as SITE_NAME -> pass on to pull info from Database
+                marker.setTag(siteNameText);
             }
 
             cursor.close();
             db.close();
-
 
         } catch(SQLiteException e) {
             Log.v("SQLiteException", "..........SQLITE_EXCEPTION..........");
@@ -162,12 +143,27 @@ public class RoamCampusMapsActivity extends FragmentActivity implements
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
+    public boolean onMarkerClick(final Marker marker) {
+        //Returning 'false' falls back to default (displaying default window)
+        return false;
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    public void onInfoWindowClick(Marker marker) {
+        String site_name = marker.getTag().toString();
+        Intent intent = new Intent(RoamCampusMapsActivity.this, RoamCampusSitesItemActivity.class);
+        intent.putExtra(RoamCampusSitesItemActivity.ITEM, site_name);
+        startActivity(intent);
+
+    }
+
+    @Override
+    public void onInfoWindowClose(Marker marker) {
+        //Do nothing
+    }
+
+    @Override
+    public void onInfoWindowLongClick(Marker marker) {
+        //Do nothing
     }
 }
---
+++ HCH/app/src/main/java/com/enm/hch/RoamCampusSitesItemActivity.java
@@ -0,0 +1,82 @@
+package com.enm.hch;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class RoamCampusSitesItemActivity extends Activity {
+
+    public static final String ITEM = "item";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sites_item);
+
+        String item = (String)getIntent().getExtras().get(ITEM);
+
+        try{
+            SQLiteOpenHelper HCHDatabaseHelper = new HCHDatabaseHelper(this);
+            SQLiteDatabase db = HCHDatabaseHelper.getReadableDatabase();
+
+            Cursor cursor = db.query ("SITES",
+                    new String[] {"_id", "SITE_NAME", "DATE_BUILT", "DATE_DESTROYED", "DESCRIPTION", "NAMESAKE"},
+                    "SITE_NAME = ?",
+                    new String[] {item},
+                    null, null, null);
+
+            //Move to the first record in the Cursor
+            if (cursor.moveToFirst()) {
+                //Get details
+                String siteNameText = cursor.getString(1);
+                String dateBuiltText = Integer.toString(cursor.getInt(2));
+                String dateDestroyedText = Integer.toString(cursor.getInt(3));
+                String descriptionText = cursor.getString(4);
+                String namesakeText = cursor.getString(5);
+
+                //Populate Site_Name
+                TextView siteName = (TextView) findViewById(R.id.site_name);
+                siteName.setText(siteNameText);
+
+                //Populate Date_Built
+                TextView dateBuilt = (TextView) findViewById(R.id.date_built);
+                dateBuilt.setText("Built: " + dateBuiltText);
+
+                //Populate Date_Destroyed
+                if(dateDestroyedText.equals("12345")) {
+                    TextView dateDestroyed = (TextView) findViewById(R.id.date_destroyed);
+                    dateDestroyed.setText("Currently Standing");
+                }
+                else {
+                    TextView dateDestroyed = (TextView) findViewById(R.id.date_destroyed);
+                    dateDestroyed.setText("Destroyed: " + dateDestroyedText);
+                }
+
+                //Populate Description
+                TextView description = (TextView) findViewById(R.id.description);
+                description.setText("\n" + descriptionText);
+
+                //Populate Namesake
+                if (namesakeText != "None") {
+                    TextView namesake = (TextView) findViewById(R.id.namesake);
+                    namesake.setText("Namesake: " + namesakeText);
+                }
+            }
+
+            cursor.close();
+            db.close();
+
+        } catch(SQLiteException e) {
+            Log.v("SQLiteException", "..........SQLITE_EXCEPTION..........");
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+}
